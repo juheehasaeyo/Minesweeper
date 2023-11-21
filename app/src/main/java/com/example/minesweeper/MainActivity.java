c846a832d24584dad2c01b3ef61e8cea13a50a7c
조주희 <carla_cho@naver.com>
2023-11-21 15:27:54 +0900

    Game Control

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
@@ -10,20 +12,83 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.content.Context;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private static int flags;
+    private static int flags = 10; // 초기 깃발 수
+    private static int mines = 10;  // 초기 지뢰 수
     private static int blocks;
     ToggleButton toggleButton;
+    public static BlockButton[][] buttons;
+    TextView minesTextView;
+
+    // 블록 열기 메소드
+    public boolean breakBlock(BlockButton button) {
+        if (button.flag) {
+            // 깃발이 꽂힌 블록을 클릭하면 아무 일도 일어나지 않도록
+            return false;
+        }
+        button.setClickable(false); // 블록을 클릭 안되는 상태로 변경
+        button.setBackgroundColor(Color.WHITE);
+
+        if (button.mine) {
+            // 지뢰인 경우
+            button.setText("\uD83D\uDCA3"); // 지뢰 표시
+            handleGameOver();
+            setAllBlocksClickable(false);  // 게임 오버 시 모든 블록 비활성화
+            return true; // 지뢰를 클릭한 경우 true 반환
+        } else {
+            if (button.neighborMines == 0) {
+                // 주변에 지뢰가 없는 경우 재귀적으로 주변 블록 열기
+                button.openNeighborBlocks(button.x, button.y, buttons);
+            } else {
+                // 주변에 지뢰가 있는 경우 주변 지뢰 수 표시
+                button.setText(String.valueOf(button.neighborMines));
+            }
+
+            button.setEnabled(false); // 블록 열린 상태로 표시
+            blocks--; // 남은 블록 수 감소
+
+            if (blocks == 0) {
+                handleGameWin();
+            }
+            return false; // 지뢰가 아닌 경우 false 반환
+        }
+    }
+
+    // 게임 종료 처리
+    private void handleGameOver() {
+        Toast.makeText(MainActivity.this, "Game Over!", Toast.LENGTH_SHORT).show();
+    }
+
+    // 게임 승리 처리
+    private void handleGameWin() {
+        Toast.makeText(MainActivity.this, "You Win!", Toast.LENGTH_SHORT).show();
+        finish(); // 액티비티 종료
+    }
+
+    // 모든 블록을 클릭이 안되게 변경
+    private void setAllBlocksClickable(boolean clickable) {
+        TableLayout table = findViewById(R.id.tableLayout);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            for (int j = 0; j < row.getChildCount(); j++) {
+                BlockButton blockButton = (BlockButton) row.getChildAt(j);
+                blockButton.setClickable(clickable);
+            }
+        }
+    }
+
     public class BlockButton extends Button {
         private int x, y;
         private boolean mine;
         private boolean flag;
         private int neighborMines;
+
         public BlockButton(Context context, int x, int y) {
             super(context);
             this.x = x;
@@ -46,37 +111,60 @@ public class MainActivity extends AppCompatActivity {
                     if (toggleButton.isChecked()) {
                         toggleFlag();
                     } else {
-                        breakBlock(view);
+                        breakBlock(buttons[x][y]);
                     }
                 }
             });
         }
+
         // 깃발 토글 메소드
         public void toggleFlag() {
-            flag = !flag;
-            if (flag) {
-                setText("\uD83D\uDEA9"); // 깃발 표시
-                flags++;
-            } else {
-                setText(""); // 깃발 해제
-                flags--;
+            if (!flag && flags > 0) {
+                // 깃발을 토글하고 남은 깃발 수가 0보다 큰 경우에만 토글 수행
+                flag = !flag;
+                if (flag) {
+                    setText("\uD83D\uDEA9"); // 깃발 표시
+                    flags--;
+                } else {
+                    setText(""); // 깃발 해제
+                    flags++;
+                }
+                // Mines 텍스트 업데이트
+                updateMinesText();
             }
         }
 
-        // 블록 열기 메소드
-        public boolean breakBlock(View view) {
-            setClickable(false); // 블록을 클릭 안되는 상태로 변경
+        // Mines 텍스트 업데이트 메소드
+        private void updateMinesText() {
+            minesTextView.setText("Mines : " + flags);
+        }
 
-            if (mine) {
-                // 지뢰인 경우
-                setText("\uD83D\uDCA3"); // 지뢰 표시
-                return true; // 지뢰를 클릭한 경우 true 반환
-            } else {
-                // 지뢰가 아닌 경우
-                setText(String.valueOf(neighborMines)); // 주변 지뢰 수 표시
-                setEnabled(false); // 블록 열린 상태로 표시
-                blocks--; // 남은 블록 수 감소
-                return false; // 지뢰가 아닌 경우 false 반환
+        // 주변 블록 열기 (재귀 호출)
+        private void openNeighborBlocks(int x, int y, BlockButton[][] buttons) {
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    int newX = x + dx;
+                    int newY = y + dy;
+
+                    // 주변에 있는 블록의 좌표가 유효한지 확인
+                    if (newX >= 0 && newX < 9 && newY >= 0 && newY < 9) {
+                        BlockButton neighborBlock = buttons[newX][newY];
+
+                        // 주변 블록이 클릭되지 않았고, 깃발이 꽂힌 블록이 아니라면
+                        if (neighborBlock != null && neighborBlock.isClickable() && !neighborBlock.flag) {
+                            // 해당 블록을 열기
+                            if (breakBlock(neighborBlock)) {
+                                // 만약 열린 블록이 지뢰라면 재귀 호출하지 않음
+                                continue;
+                            }
+
+                            // 주변에 지뢰가 없다면 해당 블록 주변의 블록들도 열기 (재귀 호출)
+                            if (neighborBlock.neighborMines == 0) {
+                                openNeighborBlocks(newX, newY, buttons);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -87,13 +175,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toggleButton = findViewById(R.id.toggleButton);
+        minesTextView = findViewById(R.id.textView);
 
-        TextView textView = findViewById(R.id.textView);
-        int mines = 10;
-        textView.setText("Mines : " + mines);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    // On 상태: 깃발 모드
+                    Toast.makeText(MainActivity.this, "깃발 모드", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Off 상태: 블록 열기 모드
+                    Toast.makeText(MainActivity.this, "블록 열기 모드", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         TableLayout table = findViewById(R.id.tableLayout);
-        BlockButton[][] buttons = new BlockButton[9][9];
+        buttons = new BlockButton[9][9];
 
         // 지뢰 배치
         ArrayList<Pair<Integer, Integer>> mineCoordinates = new ArrayList<>();
